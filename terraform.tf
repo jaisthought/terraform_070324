cfa16d04521d46fba89026eb858a901a9045d39a
jaisthought <36197362+jaisthought@users.noreply.github.com>
2024-03-07 12:59:28 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -13,9 +13,9 @@ provider "aws" {
 
 resource "aws_instance" myserver1 { 
   ami           = "ami-0f403e3180720dd7e" 
-  instance_type = "t2.micro"
+  instance_type = "t3.nano"
 
   tags = { 
-    Name = "Dev" 
+    Name = "Dev1" 
   } 
 } 
