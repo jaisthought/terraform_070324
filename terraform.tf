8753eeb30903b744790967e4e67c25f4de86ac10
jaisthought <36197362+jaisthought@users.noreply.github.com>
2024-03-07 12:55:05 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
   region = "us-east-01"
 }
+
+resource "aws_instance" myserver1 { 
+  ami           = "ami-0f403e3180720dd7e" 
+  instance_type = "t2.micro"
+
+  tags = { 
+    Name = "Dev" 
+  } 
+} 
